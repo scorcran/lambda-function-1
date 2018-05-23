43e64f5a1a13046900dd2411e9509b07461f4dbe
Sean Corcran <sean.corcran@airbnb.com>
2018-05-23 10:59:43 -0700

    second commit

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-FILENAME=$1
-
-docker build -t python-lambda .
-
-docker run -it --rm --name py-lamb -v "$PWD/src/package:/zipped-package" python-lambda $FILENAME
--- src/package.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-FILENAME=$1
-
-cd /app/src
-zip "/zipped-package/$FILENAME.zip" "$FILENAME.py"
-cd /app/src/package/
-zip -ur "/zipped-package/$FILENAME.zip" vendored
-exit 1
